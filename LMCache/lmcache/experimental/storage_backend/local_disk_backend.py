f959381c3f21956c9326110906ac7fce5881cad6
RS <rspiegelman@nvidia.com>
2025-11-10 16:45:49 +0200

    simplified the local disk backend
    Signed-off-by: RS <rspiegelman@nvidia.com>

--- LMCache/lmcache/experimental/storage_backend/local_disk_backend.py
+++ LMCache/lmcache/experimental/storage_backend/local_disk_backend.py
@@ -12,7 +12,7 @@ BRIGHT_YELLOW = "\033[93m"
 RESET = "\033[0m"
 BRIGHT_RED = "\033[91m"
 
-from collections import OrderedDict
+from collections import OrderedDict, defaultdict
 from concurrent.futures import Future
 from typing import TYPE_CHECKING, List, Optional
 import global_vars 
@@ -57,97 +57,21 @@ class LocalDiskBackend(StorageBackendInterface):
     ):
 
         log_to_pid_file(f"INIT LOCAL DISK BACKEND")
-        self.dict: OrderedDict[CacheEngineKey,
-                               DiskCacheMetadata] = OrderedDict()
-        self.dst_device = dst_device
-
-        self.prefetched = {} # key->mem_obj
-
+        self.prefetched = defaultdict(lambda: None) # key->mem_obj
         self.disk_lock = threading.Lock()
         assert config.local_disk is not None
         self.path: str = config.local_disk
         if not os.path.exists(self.path):
-            os.makedirs(self.path)
-            logger.info(f"Created local disk cache directory: {self.path}")
-
-        self.lookup_server = lookup_server
-
-        # Initialize the evictor
-        self.evictor = LRUEvictor(max_cache_size=config.max_local_disk_size)
-
+            log_to_pid_file("ERROR:BAD FILE")
         self.loop = loop
-        self.put_tasks: List[CacheEngineKey] = []
-
         self.memory_allocator = memory_allocator
-
-        self.lmcache_worker = lmcache_worker
-        self.instance_id = config.lmcache_instance_id
-        self.stats_monitor = LMCStatsMonitor.GetOrCreate()
-        self.usage = 0
 
     def __str__(self):
         return self.__class__.__name__
 
-    def _key_to_path(
-        self,
-        key: CacheEngineKey,
-    ) -> str:
-        return self.path + key.to_string().replace("/", "-") + ".pt"
-
     def contains(self, key: CacheEngineKey) -> bool:
         with self.disk_lock:
-            print(f"{BRIGHT_YELLOW}LocalDiskBackend::contains called for key {key.to_string()} is_exist = {key in self.dict}{RESET}")
-            return (key in self.dict) or (key in self.prefetched)
-    '''
-    def contains(self, key: CacheEngineKey) -> bool:
-        with self.disk_lock:
-            print(f"CCCCCCCCCCCCCCCCC CONTAINS 1  for key {key.to_string()} is_exist = {len(self.dict)}")
-            return len(self.dict) > 0
-    '''
-
-    def exists_in_put_tasks(self, key: CacheEngineKey) -> bool:
-        with self.disk_lock:
-            return key in self.put_tasks
-
-    def remove(
-        self,
-        key: CacheEngineKey,
-    ) -> None:
-        path = self.dict[key].path
-        self.disk_lock.acquire()
-        self.dict.pop(key)
-        self.disk_lock.release()
-        size = os.path.getsize(path)
-        self.usage -= size
-        self.stats_monitor.update_local_storage_usage(self.usage)
-        os.remove(path)
-
-        # push kv evict msg
-        if self.lmcache_worker is not None:
-            self.lmcache_worker.put_msg(
-                KVEvictMsg(self.instance_id, key.worker_id, key.chunk_hash,
-                           "disk"))
-
-    def insert_key(self, key: CacheEngineKey, memory_obj: MemoryObj) -> None:
-        path = self._key_to_path(key)
-        size = memory_obj.get_size()
-        shape = memory_obj.metadata.shape
-        dtype = memory_obj.metadata.dtype
-
-        has_stored = False
-        with self.disk_lock:
-            # Need to do reinsert to update cache recency
-            if key in self.dict:
-                self.dict.pop(key)
-                has_stored = True
-
-            self.dict[key] = DiskCacheMetadata(path, size, shape, dtype)
-
-        # push kv admit msg
-        if self.lmcache_worker is not None and not has_stored:
-            self.lmcache_worker.put_msg(
-                KVAdmitMsg(self.instance_id, key.worker_id, key.chunk_hash,
-                           "disk"))
+            return key in self.prefetched
 
     def submit_put_task(
         self,
@@ -156,86 +80,12 @@ class LocalDiskBackend(StorageBackendInterface):
     ) -> Optional[Future]:
         log_to_pid_file(f"IN SUBMIT PUT TASK key = {key}")
         assert memory_obj.tensor is not None
-        # print(f"{BRIGHT_YELLOW}LocalDiskBackend::submit_put_task called for key {key.to_string()}{RESET}")
         global_vars.chunk_hashes_of_curr_batch.append(key)        
-        # Update cache recency
-        evict_keys, put_status = self.evictor.update_on_put(
-            self.dict, memory_obj.get_physical_size())
-        if put_status == PutStatus.ILLEGAL:
-            return None
-        # evict caches
-        for evict_key in evict_keys:
-            self.remove(evict_key)
-        if self.lookup_server is not None:
-            self.lookup_server.batched_remove(evict_keys)
-
         self.memory_allocator.ref_count_up(memory_obj)
-
-        self.disk_lock.acquire()
-        self.put_tasks.append(key)
-        self.disk_lock.release()
-
         future = asyncio.run_coroutine_threadsafe(
             self.async_save_bytes_to_disk(key, memory_obj), self.loop)
-        # print(f"{BRIGHT_YELLOW}LocalDiskBackend::submit_put_task returning future for key {key.to_string()}{RESET}")
         return future
     
-    def submit_prefetch_task(
-        self,
-        key: CacheEngineKey,
-    ) -> Optional[Future]:
-        # print(f"{BRIGHT_YELLOW}LocalDiskBackend::submit_prefetch_task called for key {key.to_string()}{RESET}")
-
-        self.disk_lock.acquire()
-        if key not in self.dict:
-            self.disk_lock.release()
-            return None
-
-        # Update cache recency
-        self.evictor.update_on_hit(key, self.dict)
-
-        path = self.dict[key].path
-        dtype = self.dict[key].dtype
-        shape = self.dict[key].shape
-        self.disk_lock.release()
-        logger.info(f"Prefetching {key} from disk.")
-
-        assert dtype is not None
-        assert shape is not None
-        future = asyncio.run_coroutine_threadsafe(
-            self.async_load_bytes_from_disk(path, dtype, shape), self.loop)
-        # print(f"{BRIGHT_YELLOW}LocalDiskBackend::submit_prefetch_task returning future for key {key.to_string()}{RESET}")
-        return future 
-
-    def prefetch(
-        self,
-        key: CacheEngineKey,
-    ) -> Optional[Future]:
-        print(f"{BRIGHT_YELLOW}LocalDiskBackend::prefetch called for key {key.to_string()}{RESET}")
-
-        self.disk_lock.acquire()
-        if key not in self.dict:
-            self.disk_lock.release()
-            return (None, None)
-
-        # Update cache recency
-        self.evictor.update_on_hit(key, self.dict)
-
-        path = self.dict[key].path
-        dtype = self.dict[key].dtype
-        shape = self.dict[key].shape
-        self.disk_lock.release()
-        # logger.info(f"Prefetching {key} from disk.")
-
-        assert dtype is not None
-        assert shape is not None
-        future = asyncio.run_coroutine_threadsafe(
-            self.async_load_bytes_from_disk(path, dtype, shape), self.loop)
-        print(f"{BRIGHT_YELLOW}LocalDiskBackend::prefetch returning future for key {key.to_string()}{RESET}")
-        return (future, key)
-
-
-
 
     async def prefetch_async(self, keys):
         print(f"{BRIGHT_YELLOW}LocalDiskBackend::prefetch called for keys {keys}{RESET}")
@@ -267,18 +117,6 @@ class LocalDiskBackend(StorageBackendInterface):
             del self.prefetched[key] 
         self.disk_lock.release()
 
- 
-    def feed_prefetched(self, prefteched):
-        self.disk_lock.acquire()
-        self.prefetched = prefteched
-        self.disk_lock.release()
-
-
-    def free_prefetched(self, key = None):
-        self.disk_lock.acquire()
-        for key in self.prefetched:
-            self.memory_allocator.free(self.prefetched[key])
-        self.disk_lock.release()
 
     def get_blocking(
         self,
@@ -289,32 +127,8 @@ class LocalDiskBackend(StorageBackendInterface):
         """
         print(f"{BRIGHT_YELLOW}LocalDiskBackend::get_blocking called for key {key.to_string()}{RESET}")
         log_to_pid_file(f"IN GET BLOCKING key = {key}")
-        self.disk_lock.acquire()
-        if key in self.prefetched:
-            ret = self.prefetched[key]
-            self.disk_lock.release()
-            print(f"{BRIGHT_GREEN}LocalDiskBackend::get_blocking serving from prefetched KV's{RESET}")
-            return ret
-
-
-        if key not in self.dict:
-            print(f"{BRIGHT_RED}LocalDiskBackend::get_blocking key not in dict{RESET}")
-            self.disk_lock.release()
-            return None
-        
-
-        # Update cache recency
-        self.evictor.update_on_hit(key, self.dict)
-
-        path = self.dict[key].path
-        dtype = self.dict[key].dtype
-        shape = self.dict[key].shape
-        assert dtype is not None
-        assert shape is not None
-        memory_obj = self.load_bytes_from_disk(path, dtype=dtype, shape=shape)
-        self.disk_lock.release()
-        print(f"{BRIGHT_YELLOW}LocalDiskBackend::get_blocking returning present for key {key.to_string()}{RESET}")
-        return memory_obj
+        with self.disk_lock:
+            return self.prefetched[key]
     
     
 
@@ -328,24 +142,12 @@ class LocalDiskBackend(StorageBackendInterface):
         """
         Convert KV to bytes and async store bytes to disk.
         """
-        kv_chunk = memory_obj.tensor
-        assert kv_chunk is not None
         byte_array = memory_obj.byte_array
-        path = self._key_to_path(key)
-
-        size = len(byte_array)
-        self.usage += size
-        self.stats_monitor.update_local_storage_usage(self.usage)
-
-        async with aiofiles.open(path, 'wb') as f:
+        async with aiofiles.open(self.path, 'r+b') as f:
+            await f.seek(0)  # Move to offset 0
             await f.write(byte_array)
-
-        self.insert_key(key, memory_obj)
         self.memory_allocator.ref_count_down(memory_obj)
 
-        self.disk_lock.acquire()
-        self.put_tasks.remove(key)
-        self.disk_lock.release()
 
     # TODO(Jiayi): use `bytes_read = await f.readinto(buffer)`
     # for better performance (i.e., fewer copy)
@@ -364,52 +166,5 @@ class LocalDiskBackend(StorageBackendInterface):
             return None
         buffer = memory_obj.byte_array
         async with aiofiles.open(path, 'rb') as f:
-            await f.readinto(buffer) #we will need to do here pread intead of read, with the block device offset, also add a "header" with our hash to know it this is our info or not
+            await f.readinto(buffer) 
         return memory_obj
-
-    # TODO(Jiayi): use memory allocator to redeuce cpu buffer allocation
-    # TODO(Jiayi): the pinned cpu memory_obj should directly be passed into
-    # gpu connector; this gpu buffer could be avoided
-    def load_bytes_from_disk(
-        self,
-        path: str,
-        dtype: torch.dtype,
-        shape: torch.Size,
-    ) -> Optional[MemoryObj]:
-        """
-        Load bytearray from disk.
-        """
-        memory_obj = self.memory_allocator.allocate(shape, dtype)
-        if memory_obj is None:
-            logger.debug("Memory allocation failed during async disk load.")
-            return None
-        buffer = memory_obj.byte_array
-        with open(path, 'rb') as f:
-            f.readinto(buffer)
-        return memory_obj
-
-    @_lmcache_nvtx_annotate
-    @torch.inference_mode()
-    def load_disk(
-        self,
-        path: str,
-        backend: str = "bytes",
-        dtype: Optional[torch.dtype] = None,
-        shape: Optional[torch.Size] = None,
-    ) -> Optional[MemoryObj]:
-        """
-        Load KV from disk.
-        """
-        if backend == "bytes":
-            assert dtype is not None
-            assert shape is not None
-            memory_obj = self.load_bytes_from_disk(path, dtype, shape)
-        else:
-            raise ValueError(f"Invalid backend: {backend}")
-        return memory_obj
-
-    def close(self) -> None:
-        if self.lookup_server is not None:
-            self.disk_lock.acquire()
-            self.lookup_server.batched_remove(list(self.dict.keys()))
-            self.disk_lock.release()
